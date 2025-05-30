101c32a30cd872480ad6242f8133889dd9c5ff42
Hansi <punsandanih2005@gmail.com>
2025-05-30 19:33:01 +1000

    Add the filter for pie chart

--- js/load-data.js
+++ js/load-data.js
@@ -1,14 +1,27 @@
+let fullData = [];
+
 d3.csv("data/piechart_data.csv", d => ({
   ageGroup: d.AGE_GROUP,
   fines: +d.FINES,
   year: +d.YEAR,
   month: +d.MONTH
 })).then(data => {
-  const filteredData = data.filter(d => d.year === 2023);
-  drawPieChart(filteredData);
+  fullData = data.filter(d => d.year === 2023); // store filtered 2023 data
+
+  drawPieChart(fullData); // draw initial chart
+
+  // Add event listener for dropdown
+  d3.select("#monthFilter").on("change", function () {
+    const selected = this.value;
+    const monthFiltered = selected === "all"
+      ? fullData
+      : fullData.filter(d => d.month === +selected);
+    drawPieChart(monthFiltered);
+  });
 }).catch(error => {
   console.error("Error loading the CSV file:", error);
 });
+
 
 d3.csv("data/barchart.csv", d => ({
   year: +d.YEAR,
