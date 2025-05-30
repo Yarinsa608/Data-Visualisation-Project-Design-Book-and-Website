d7f948a6778f681d62e14e45eb1f2e907c57e3be
Hansi <punsandanih2005@gmail.com>
2025-05-30 20:41:55 +1000

    Set up the filter for the pie chart

--- js/load-data.js
+++ js/load-data.js
@@ -1,14 +1,20 @@
+let fullData = [];
+
 d3.csv("data/piechart_data.csv", d => ({
   ageGroup: d.AGE_GROUP,
   fines: +d.FINES,
   year: +d.YEAR,
   month: +d.MONTH
 })).then(data => {
-  const filteredData = data.filter(d => d.year === 2023);
-  drawPieChart(filteredData);
+  // Only use 2023 data
+  fullData = data.filter(d => d.year === 2023);
+
+  // Set up the dropdown and initial chart
+  setupPieChart(fullData);
 }).catch(error => {
   console.error("Error loading the CSV file:", error);
 });
+
 
 d3.csv("data/barchart.csv", d => ({
   year: +d.YEAR,
